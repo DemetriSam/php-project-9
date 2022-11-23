c1a59a771890cf69baf3d7d42a64ab71e9f0eb54
DemetriSam <samartsew@gmail.com>
2022-11-23 10:39:46 +0300

    step 5

--- public/index.php
+++ public/index.php
@@ -52,12 +52,18 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 
 $app->get('/urls', function (Request $request, Response $response, array $args) use ($dbconfig) {
     $pdo = connect(...$dbconfig);
-    $query = "SELECT urls.id, urls.name, MAX(url_checks.created_at) as last_check
-        FROM urls 
+    $query = 
+        "SELECT nested.id, nested.name, url_checks.status_code, nested.last_check, nested.created_at
+        FROM (	
+            SELECT urls.id, urls.name, urls.created_at, MAX(url_checks.created_at) as last_check
+                    FROM urls 
+                    LEFT JOIN url_checks
+                    ON urls.id = url_checks.url_id
+                    GROUP BY urls.id
+        ) AS nested
         LEFT JOIN url_checks
-        ON urls.id = url_checks.url_id
-        GROUP BY urls.id
-        ORDER BY urls.created_at DESC";
+        ON nested.id  = url_checks.url_id and nested.last_check = url_checks.created_at
+        ORDER BY nested.created_at DESC";
     $rows = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
     return $this->get('view')->render($response, 'urls.html', compact('rows'));
 })->setName('urls');
@@ -71,7 +77,7 @@ $app->get('/urls/{id}', function (Request $request, Response $response, array $a
     $getUrlInfo = "SELECT * FROM urls WHERE id=$id";
     $urlInfo = $pdo->query($getUrlInfo)->fetch(PDO::FETCH_ASSOC);
 
-    $getChecks = "SELECT * FROM url_checks WHERE url_id=$id";
+    $getChecks = "SELECT * FROM url_checks WHERE url_id=$id ORDER BY created_at DESC";
     $checks = $pdo->query($getChecks)->fetchAll(PDO::FETCH_ASSOC);
 
     return $this->get('view')->render($response, 'show.url.html', compact('urlInfo', 'checks', 'messages'));
@@ -120,11 +126,26 @@ $app->post('/urls', function (Request $request, Response $response, array $args)
 $app->post('/urls/{url_id}/checks', function (Request $request, Response $response, array $args) use ($dbconfig, $app) {
     $urlId = $args['url_id'];
     $now = Carbon::now()->toDateTimeString();
-    $query = "INSERT INTO url_checks (url_id, created_at) VALUES ('$urlId', '$now')";
     $pdo = connect(...$dbconfig);
+    $routeParser = $app->getRouteCollector()->getRouteParser();
+
+    $query = "SELECT name FROM urls WHERE id=$urlId";
+    $url = $pdo->query($query)->fetch()['name'];
+
+    $client = new GuzzleHttp\Client();
+
+    try {
+        $res = $client->request('GET', $url);
+    } catch (\Throwable $e) {
+        $this->get('flash')->addMessage('error', 'Произошла ошибка при проверке, не удалось подключиться');
+        return $response->withHeader('Location', $routeParser->urlFor('urls.show', ['id' => $urlId]))->withStatus(302);
+    }
+
+    $statusCode = $res->getStatusCode();
+
+    $query = "INSERT INTO url_checks (url_id, created_at, status_code) VALUES ('$urlId', '$now', $statusCode)";
     $pdo->query($query);
 
-    $routeParser = $app->getRouteCollector()->getRouteParser();
     $this->get('flash')->addMessage('success', 'Страница успешно проверена');
     return $response->withHeader('Location', $routeParser->urlFor('urls.show', ['id' => $urlId]))->withStatus(302);
 });
